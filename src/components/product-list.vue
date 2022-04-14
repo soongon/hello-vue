5f87893f5e6aa1b5253573d71a5ef59362b59ce7
soongonkim <soongon@gmal.com>
2022-04-14 14:19:41 +0900

    initial commit

--- src/components/product-list.vue
+++ src/components/product-list.vue
@@ -1,16 +1,33 @@
 <template>
+  <p>
+    <button @click="getDataFromServer()">데이터 가져오기</button>
+  </p>
   <ul>
-      <li>{{ products[0] }}</li>
-      <li>{{ products[1] }}</li>
-      <li>{{ products[2] }}</li>
+      <li v-for="post in products" :key="post.id">
+        <p>글번호: {{ post.id }}</p>
+        <p>{{ post.title }}</p>
+      </li>
     </ul>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  created() {
+    this.getDataFromServer();
+  },
+  methods: {
+    async getDataFromServer() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+          console.log(res.data);
+          this.products = res.data;
+        });
+    }
+  },
   data() {
     return {
-      products: ['아이폰13', '갤럭시22', '모니터27'],
+      products: [],
     };
   }
 }
